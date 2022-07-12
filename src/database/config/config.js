5f6795fef23a59fe66b8d8345d17a378d54b268e
Adriel23 <adrielcolombo23@gmail.com>
2022-07-12 14:35:37 -0300

    configuraciones para hacer deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "asyouwant",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port":process.env.DB_PORT,
+    "dialect": "mysql",
+    "operatorsAliases": 0
   },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "operatorsAliases": 0
+  }
 };
   // "test": {
   //   "username": "root",
@@ -14,11 +26,5 @@ module.exports = {
   //   "host": "127.0.0.1",
   //   "dialect": "mysql"
   // },
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // }
+
 
